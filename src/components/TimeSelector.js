bbdc6311d060a4f3855dfddaeff8192b1ebd2a83
Hamza Sumbal <hamzasumbal@gmail.com>
2020-12-01 09:59:42 +0500

    Haptic feedback Added + Andriod User can not see component

--- src/components/TimeSelector.js
+++ src/components/TimeSelector.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet, ScrollView, Button} from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient';
+import * as Haptics from 'expo-haptics';
 import Colors from '../constants/Colors'
 const Hours = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
 const Minutes = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",]
@@ -31,6 +32,7 @@ const TimeSelector = ({limit, defaultOffsetHour}) => {
             if (HoursYScroll > a && HoursYScroll < b) {
                 if (selectedHour !== Hours[i]) {
                     setSelectedHour(Hours[i])
+                    Haptics.selectionAsync()
                     return;
                 }
             }
@@ -55,6 +57,7 @@ const TimeSelector = ({limit, defaultOffsetHour}) => {
             if (MinutesYScroll > a && MinutesYScroll < b) {
                 if (selectedMinute !== Minutes[i]) {
                     setSelectedMinute(Minutes[i])
+                    Haptics.selectionAsync()
                     return;
                 }
             }
